b4526f760dd866603157f6055e6dd45ddb2d6ec5
khedoros <khedoros@gmail.com>
2020-05-03 13:08:50 -0600

    Fixed M68k instruction decode
    - Some instructions needed to be reordered to ensure appropriate priority (if 2 or more patterns cover the same opcode, we need the right pattern to be chosen). By changing the order that patterns are listed in, I can cause certain decodings to take priority over others.

--- cpu/m68k-instruction-decode-test.cpp
+++ cpu/m68k-instruction-decode-test.cpp
@@ -5,7 +5,7 @@
 #include "m68k_instructions.h"
 
 int main() {
-    std::vector<std::list<std::string>> types(65536, std::list<std::string>{"none"});
+    std::vector<std::list<std::string>> types(65536);
     for(int i=0;i<65536;i++) {
         for(auto& j: instrs) {
             int c = (i & (j.mask_bits));
@@ -13,7 +13,8 @@ int main() {
                 types[i].push_back(j.op_name);
             }
         }
-        std::cout<<i<<": ";
+	std::printf("%04x: ", i);
+//        std::cout<<std::hex<<i<<": ";
         for(auto& j: types[i]) {
             std::cout<<j<<", ";
         }
