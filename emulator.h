593021546e042ceafcccbf1de4c46ef3ec8bc361
khedoros <khedoros@gmail.com>
2020-05-22 15:56:01 -0600

    Thinking about emulator structure

--- emulator.h
+++ emulator.h
@@ -1,9 +1,20 @@
 #pragma once
 #include<memory>
 
-#include "io/ioMgr.h"
+//#include "io/ioMgr.h"
 
 class config;
+class ioMgr;
+class apu;
+class ti_psg;
+class yamaha_fm;
+class memmap_apu;
+class cpu_z80;
+class memmap_m68k;
+class cpu_m68k;
+class input_mapper;
+class memmap_vdp;
+class vdp;
 
 class emulator {
 public:
@@ -19,6 +30,19 @@ public:
     genesisEmulator(std::shared_ptr<config> cfg);
     int run() override;
 private:
+    std::shared_ptr<apu>            apu_dev;
+    std::shared_ptr<ti_psg>         psg_dev;
+    std::shared_ptr<yamaha_fm>      yama_dev;
+    std::shared_ptr<memmap_apu>     apu_map;
+    std::shared_ptr<cpu_z80>        apu_cpu;
+
+    std::shared_ptr<memmap_m68k>    cpu_map;
+    std::shared_ptr<cpu_m68k>       main_cpu;
+    std::shared_ptr<input_mapper>   input_map;
+    std::shared_ptr<ioMgr>          io_manager;
+
+    std::shared_ptr<memmap_vdp>     vdp_map;
+    std::shared_ptr<vdp>            vdp_dev;
 };
 
 class smsEmulator: public emulator {
--- memmap_m68k.h
+++ memmap_m68k.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include<cstdint>
+#include<array>
+#include<memory>
 
 class memmap_m68k {
 public:
-    memmap_m68k();
+    memmap_m68k(std::shared_ptr<config> config);
     uint8_t readByte(uint32_t addr);
     uint16_t readWord(uint32_t addr);
     uint32_t readLong(uint32_t addr);
@@ -12,5 +14,9 @@ public:
     void writeWord(uint32_t addr, uint16_t val);
     void writeLong(uint32_t addr, uint32_t val);
 private:
-
+    std::array<uint8_t, 0x400000> rom;
+    std::array<uint8_t, 0x10000>  ram;
+    std::shared_ptr<memmap_apu>   apu;
+    std::shared_ptr<memmap_vdp>   vdp;
+    std::shared_ptr<input_mapper> io;
 };
