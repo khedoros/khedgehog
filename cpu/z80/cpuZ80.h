f070a1a3881453f634925f040655c0238b9fd541
khedoros <khedoros@gmail.com>
2020-11-07 00:08:14 -0700

    Added dummy tables for CPU opcodes

--- cpu/z80/cpuZ80.h
+++ cpu/z80/cpuZ80.h
@@ -5,9 +5,11 @@
 #include<array>
 #include<cstdint>
 
+#define CALL_MEMBER_FN(object,ptrToMember)  ((object)->*(ptrToMember))
+
 class cpuZ80;
 
-using z80OpPtr = uint64_t (cpuZ80::*)();
+using z80OpPtr = uint64_t (cpuZ80::*)(uint8_t);
 
 class cpuZ80 : public cpu {
 private:
@@ -49,14 +51,15 @@ private:
     static std::array<z80OpPtr, 256> fd_op_table;
     static std::array<z80OpPtr, 256> fdcb_op_table;
 
-    uint64_t cb_op_prefix();
-    uint64_t dd_op_prefix();
-    uint64_t ed_op_prefix();
-    uint64_t fd_op_prefix();
-    uint64_t fdcb_op_prefix();
+    template <uint32_t OPCODE> uint64_t cb_op_prefix(uint8_t);
+    template <uint32_t OPCODE> uint64_t dd_op_prefix(uint8_t);
+    template <uint32_t OPCODE> uint64_t ed_op_prefix(uint8_t);
+    template <uint32_t OPCODE> uint64_t fd_op_prefix(uint8_t);
+    template <uint32_t OPCODE> uint64_t fdcb_op_prefix(uint8_t);
 
-    template <uint8_t OPCODE>
-    uint64_t op_unimplemented();
+    uint64_t decode(uint8_t opcode);
+
+    template <uint32_t OPCODE> uint64_t op_unimpl(uint8_t);
 
 
 
--- cpu/z80/memmapZ80.cpp
+++ cpu/z80/memmapZ80.cpp
@@ -1,1 +1,17 @@
 #include "memmapZ80.h"
+
+uint8_t& memmapZ80::readByte(uint32_t addr) {
+    return dummy8;
+}
+
+uint16_t& memmapZ80::readWord(uint32_t addr) {
+    return dummy16;
+}
+
+uint32_t& memmapZ80::readLong(uint32_t addr) {
+    return dummy32;
+}
+
+void memmapZ80::writeByte(uint32_t addr, uint8_t val) {}
+void memmapZ80::writeWord(uint32_t addr, uint16_t val) {}
+void memmapZ80::writeLong(uint32_t addr, uint32_t val) {}
