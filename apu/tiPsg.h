ae2ba37b93768e1ea60dd94a1ee8afc2fc379dd0
khedoros <khedoros@gmail.com>
2021-03-01 20:13:30 -0700

    Added more PSG info

--- apu/tiPsg.h
+++ apu/tiPsg.h
@@ -15,14 +15,18 @@ private:
     bool latchedType:1; // 0 = data, 1 = volume
     unsigned toneCountReset[4]:10; // 0 and 1 just give a constant "1" output. First audible signal on a real SMS2 was at Reset value 6, at 18643Hz
                                    // Noise channel resets: b00 = 0x10, b01 = 0x20, b02 = 0x40, b03 = value of Tone2's reset(?)
-                                   // Noise channel uses 2 different Linear-Feedback Shift Registers (looks like it starts with 1, is 15 bits wide for SG-1000,
-                                   // 16 bits for the others, shifts right, and at least on the 16-bit one, has the 13th and 16th bits tapped with a XOR.
     unsigned attenuation[4]:4; // Attenuation values would output volumes like this:
     //  int volume_table[16]={ 32767, 26028, 20675, 16422, 13045, 10362,  8231,  6568, 5193,  4125,  3277,  2603,  2067,  1642,  1304,     0};
  }
     bool currentOutput[4]; // Currently outputting low or high voltage
     unsigned noiseShiftRate:2;
     bool noiseMode; // 0 = periodic, 1 = white
+                                   // Noise channel uses 2 different Linear-Feedback Shift Registers (looks like it starts with 1, is 15 bits wide for SG-1000,
+                                   // 16 bits for the others, shifts right)
+                                   //   On the 16-bit one, has the 13th and 16th bits tapped with a XOR.
+                                   //   For the 15-bit one, the tapped bits are the 15th and 11th bits, feeding back into the first.
+                                   // For periodic "noise", it looks like it's just a frequency-divisor, with the only tap being on the highest bit, copying it back to the lowest bit.
+
 
     // Port 6 on the Game Gear, bits 0-3 are channels 0-3 on the right, bits 4-7 are channels 0-3 on the left.
     bool stereoLeft[4];
