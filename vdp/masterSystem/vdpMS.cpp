10ca88f54c01e9d913cbe7f06a14d5f5425bb435
khedoros <khedoros@gmail.com>
2021-03-11 22:15:53 -0800

    Implemented GG/SMS sprite collision and overflow

--- vdp/masterSystem/vdpMS.cpp
+++ vdp/masterSystem/vdpMS.cpp
@@ -35,9 +35,9 @@ void vdpMS::resizeBuffer(unsigned int x, unsigned int y) {
 
 vdpMS::graphicsMode_t vdpMS::getMode() {
     int mode = 8 * ctrl_1.fields.mode_4 +
-               4 * ctrl_2.fields.mode_3 +
-               2 * ctrl_1.fields.mode_2 +
-                   ctrl_2.fields.mode_1;
+        4 * ctrl_2.fields.mode_3 +
+        2 * ctrl_1.fields.mode_2 +
+        ctrl_2.fields.mode_1;
     switch(mode) {
         case 0: return graphicsMode_t::graphics1;
         case 1: return graphicsMode_t::text;
@@ -118,50 +118,50 @@ void vdpMS::renderLine(unsigned int line, std::vector<uint8_t>& renderBuffer) {
 std::vector<uint8_t> vdpMS::getPartialRender() {
     std::vector<uint8_t> buffer(192 * 256 * 3, 0);
     if(!ctrl_2.fields.enable_display) return buffer;
-	for(int i=0;i<192;i++) renderLine(i, buffer);
+    for(int i=0;i<192;i++) renderLine(i, buffer);
     return buffer;
 }
 
 void vdpMS::renderGraphic1(unsigned int line, std::vector<uint8_t>& buffer) {
-	std::cout<<"Graphic I (Mode 0)  render\n";
-	int y_tile = line / 8;
-	int y_line = line % 8;
-	for(int x_tile=0;x_tile<32;x_tile++) {
-		int tile_num_addr = (name_tab_base() + y_tile * 32 + x_tile) & 0x3fff;
-		int tile_num = vram.at(tile_num_addr);
-		int tile_addr = (bg_tile_base() + tile_num * 8) & 0x3fff;
+    std::cout<<"Graphic I (Mode 0)  render\n";
+    int y_tile = line / 8;
+    int y_line = line % 8;
+    for(int x_tile=0;x_tile<32;x_tile++) {
+        int tile_num_addr = (name_tab_base() + y_tile * 32 + x_tile) & 0x3fff;
+        int tile_num = vram.at(tile_num_addr);
+        int tile_addr = (bg_tile_base() + tile_num * 8) & 0x3fff;
 
-		int color_addr = (col_tab_base() + tile_num / 8) & 0x3fff;
-		bg_fg_col_t colors{.val = vram.at(color_addr)};
+        int color_addr = (col_tab_base() + tile_num / 8) & 0x3fff;
+        bg_fg_col_t colors{.val = vram.at(color_addr)};
 
-		auto tileLine = getG2TileLine(tile_addr, y_line);
-		for(int x = 0; x < 8; x++) {
-			uint8_t color_index = tileLine[x] ? colors.fields.foreground: colors.fields.background;
-			setPixelSG(buffer, x_tile * 8 + x, y_tile * 8 + y_line, color_index);
-		}
-	}
+        auto tileLine = getG2TileLine(tile_addr, y_line);
+        for(int x = 0; x < 8; x++) {
+            uint8_t color_index = tileLine[x] ? colors.fields.foreground: colors.fields.background;
+            setPixelSG(buffer, x_tile * 8 + x, y_tile * 8 + y_line, color_index);
+        }
+    }
 }
 
 void vdpMS::renderGraphic2(unsigned int line, std::vector<uint8_t>& buffer) {
-	//std::cout<<"Graphic II (Mode 2) render ntbase: "<<name_tab_base()<<" ttbase: "<<bg_tile_base()<<" ctbase: "<<col_tab_base()<<"\n";
-	int yTile = line / 8;
-	int yTriad = yTile / 8;
-	int yLine = line % 8;
-	for(int x_tile=0;x_tile<32;x_tile++) {
-		int tile_num_addr = (name_tab_base() + yTile * 32 + x_tile) & 0x3fff;
-		int tile_num = vram.at(tile_num_addr) + 256 * yTriad;
-		int tile_addr = ((bg_tile_base() & 0x2000) + tile_num * 8) & 0x3fff;
+    //std::cout<<"Graphic II (Mode 2) render ntbase: "<<name_tab_base()<<" ttbase: "<<bg_tile_base()<<" ctbase: "<<col_tab_base()<<"\n";
+    int yTile = line / 8;
+    int yTriad = yTile / 8;
+    int yLine = line % 8;
+    for(int x_tile=0;x_tile<32;x_tile++) {
+        int tile_num_addr = (name_tab_base() + yTile * 32 + x_tile) & 0x3fff;
+        int tile_num = vram.at(tile_num_addr) + 256 * yTriad;
+        int tile_addr = ((bg_tile_base() & 0x2000) + tile_num * 8) & 0x3fff;
 
-		int color_addr = ((col_tab_base() & 0x2000) + tile_num * 8) & 0x3fff ;
+        int color_addr = ((col_tab_base() & 0x2000) + tile_num * 8) & 0x3fff ;
 
-		bg_fg_col_t colors{.val = vram.at(color_addr + yLine)};
-		auto line = getG2TileLine(tile_addr, yLine);
-		for(int x = 0; x < 8; x++) {
-			uint8_t color_index = line[x] ? colors.fields.foreground : colors.fields.background;
-			setPixelSG(buffer, x_tile * 8 + x, yTile * 8 + yLine, color_index);
-		}
-	}
-        renderSgSprites(line, buffer);
+        bg_fg_col_t colors{.val = vram.at(color_addr + yLine)};
+        auto line = getG2TileLine(tile_addr, yLine);
+        for(int x = 0; x < 8; x++) {
+            uint8_t color_index = line[x] ? colors.fields.foreground : colors.fields.background;
+            setPixelSG(buffer, x_tile * 8 + x, yTile * 8 + yLine, color_index);
+        }
+    }
+    renderSgSprites(line, buffer);
 }
 
 void vdpMS::renderSgSprites(unsigned int line, std::vector<uint8_t>& buffer) {
@@ -187,23 +187,23 @@ void vdpMS::renderSgSprites(unsigned int line, std::vector<uint8_t>& buffer) {
         }
     }
 
-	// Check collision
-	std::vector<uint8_t> sprCollis(sprCount);
-	for(int i=0;i<sprCount;i++) {
-		int sprX = vram.at(sprAttrTabAddr + sprSearch[i] * 4 + 1); // sprite X coordinate
-		int info = vram.at(sprAttrTabAddr + sprSearch[i] * 4 + 3); // sprite info
+    // Check collision
+    std::vector<uint8_t> sprCollis(sprCount);
+    for(int i=0;i<sprCount;i++) {
+        int sprX = vram.at(sprAttrTabAddr + sprSearch[i] * 4 + 1); // sprite X coordinate
+        int info = vram.at(sprAttrTabAddr + sprSearch[i] * 4 + 3); // sprite info
         if(info & 0x80) sprX -= 32;
-		sprCollis[i] = sprX;
-	}
-	std::sort(sprCollis.begin(), sprCollis.end());
-	int first = 0;
-	if(sprCount > 0) first = sprCollis[0];
-	for(int i=1;i<sprCount;i++) {
-		if(sprCollis[i] - first < sprHeight) status.fields.collision_flag = 1;
-		first = sprCollis[i];
-	}
+        sprCollis[i] = sprX;
+    }
+    std::sort(sprCollis.begin(), sprCollis.end());
+    int first = 0;
+    if(sprCount > 0) first = sprCollis[0];
+    for(int i=1;i<sprCount;i++) {
+        if(sprCollis[i] - first < sprHeight) status.fields.collision_flag = 1;
+        first = sprCollis[i];
+    }
 
-	// Draw the sprites
+    // Draw the sprites
     int tileRepeat = ctrl_2.fields.large_sprites + 1; // 4 tiles drawn in a square
     int pixelRepeat = ctrl_2.fields.doubled_sprites + 1; // tile pixels are doubled
 
@@ -218,8 +218,8 @@ void vdpMS::renderSgSprites(unsigned int line, std::vector<uint8_t>& buffer) {
 
         int sprLine = line - sprY;
         if(ctrl_2.fields.doubled_sprites) {
-			sprLine /= 2;
-		}
+            sprLine /= 2;
+        }
         if(ctrl_2.fields.large_sprites && sprLine > 7) {
             tile++; // vertical tile increment
             sprLine -= 8;
@@ -234,7 +234,7 @@ void vdpMS::renderSgSprites(unsigned int line, std::vector<uint8_t>& buffer) {
             }
             tile += 2; // horizontal tile increment
         }
-        
+
     }
 }
 
@@ -242,7 +242,7 @@ void vdpMS::renderSmsSprites(unsigned int line, std::vector<uint8_t>& buffer) {
 
 }
 
-    // 
+// 
 
 void vdpMS::renderText(unsigned int line, std::vector<uint8_t>& buffer) {
     std::cout<<"Text (Mode 1) render\n";
@@ -254,102 +254,112 @@ void vdpMS::renderMulticolor(unsigned int line, std::vector<uint8_t>& buffer) {
 
 void vdpMS::renderMode4(unsigned int line, std::vector<uint8_t>& buffer) {
 
-	int scrYStart = 0, scrYEnd = 192, scrXStart = 0, scrXEnd = 256;
-	if(vdpMode == systemType::gameGear) {
-		scrYStart = 3 * 8;
-		scrYEnd = 192 - 3 * 8;
-		scrXStart = 6 * 8;
-		scrXEnd = 256 - 6 * 8;
-	}
+    int scrYStart = 0, scrYEnd = 192, scrXStart = 0, scrXEnd = 256;
+    if(vdpMode == systemType::gameGear) {
+        scrYStart = 3 * 8;
+        scrYEnd = 192 - 3 * 8;
+        scrXStart = 6 * 8;
+        scrXEnd = 256 - 6 * 8;
+    }
 
-	if(line < scrYStart || line >= scrYEnd) return;
+    if(line < scrYStart || line >= scrYEnd) return;
 
-	int sprHeight = (ctrl_2.fields.large_sprites)? 16 : 8;
+    int sprHeight = (ctrl_2.fields.large_sprites)? 16 : 8;
 
-	// Argument is `line` for consistency with other functions,
-	// but "scrY" was the original loop variable when this function wasn't line-based
-	int scrY = line;
+    // Argument is `line` for consistency with other functions,
+    // but "scrY" was the original loop variable when this function wasn't line-based
+    int scrY = line;
 
-	int bgY = (scrY + bg_y_scroll) % (28*8);
-	int yTile = bgY / 8;
-	int yFine = bgY % 8;
+    int bgY = (scrY + bg_y_scroll) % (28*8);
+    int yTile = bgY / 8;
+    int yFine = bgY % 8;
 
-	std::array<int, 8> sprSearch;
-	int sprCount = 0;
-	for(int i = 0; vram.at(sprite_attr_tab_base() + i) != 0xd0 && i < 64;i++) {
-		int y = vram.at(sprite_attr_tab_base() + i) + 1;
-		if(scrY - y >= 0 && scrY - y < sprHeight) sprSearch[sprCount++] = i;
-		if(sprCount == 8) break;
-	}
+    std::array<int, 8> sprSearch;
+    int sprCount = 0;
+    for(int i = 0; vram.at(sprite_attr_tab_base() + i) != 0xd0 && i < 64;i++) {
+        int y = vram.at(sprite_attr_tab_base() + i) + 1;
+        if(scrY - y >= 0 && scrY - y < sprHeight) {
+            if(sprCount == 8) {
+                status.fields.overflow_flag = 1;
+                break;
+            }
+            else {
+                sprSearch[sprCount++] = i;
+            }
+        }
+    }
 
-	std::array<int, 256> lineBuffer{0};
+    std::array<int, 256> lineBuffer{0};
 
-	// Draw sprites into lineBuffer
-	for(int spr = 0; spr < sprCount; spr++) {
-		int y = vram.at(sprite_attr_tab_base() + sprSearch[spr]) + 1;
-		int x = vram.at(sprite_attr_tab_base() + 128 + sprSearch[spr] * 2);
-		int tile = vram.at(sprite_attr_tab_base() + 128 + sprSearch[spr] * 2 + 1);
-		int fineY = scrY - y;
-		//std::cout<<"Sprite#: "<<spr<<" Line: "<<scrY<<" Spr_y: "<<y
-		assert(fineY >= 0);
-		assert(fineY < sprHeight);
+    // Draw sprites into lineBuffer
+    for(int spr = 0; spr < sprCount; spr++) {
+        int y = vram.at(sprite_attr_tab_base() + sprSearch[spr]) + 1;
+        int x = vram.at(sprite_attr_tab_base() + 128 + sprSearch[spr] * 2);
+        int tile = vram.at(sprite_attr_tab_base() + 128 + sprSearch[spr] * 2 + 1);
+        int fineY = scrY - y;
+        //std::cout<<"Sprite#: "<<spr<<" Line: "<<scrY<<" Spr_y: "<<y
+        assert(fineY >= 0);
+        assert(fineY < sprHeight);
 
-		uint16_t tile_addr = (sprite_tile_base() + 32 * tile) & 0x3fff;
-		auto line = getM4TileLine(tile_addr, fineY);
+        uint16_t tile_addr = (sprite_tile_base() + 32 * tile) & 0x3fff;
+        auto line = getM4TileLine(tile_addr, fineY);
 
-		for(int xFine = 0; xFine < 8 && xFine + x < 256; xFine++) {
-			int color_index = line[xFine] + 16;
-			if(color_index != 16) {
-				lineBuffer[x + xFine] = color_index;
-			}
-		}
-	}
+        for(int xFine = 0; xFine < 8 && xFine + x < 256; xFine++) {
+            int color_index = line[xFine] + 16;
+            if(color_index != 16) {
+                if(lineBuffer[x + xFine] != 0)  {
+                    status.fields.collision_flag = 1;
+                }
+                lineBuffer[x + xFine] = color_index;
+            }
+        }
+    }
 
-	for(int scrX = scrXStart; scrX < scrXEnd; scrX++) {
-		//          TODO: Use a similar algorithm in endLine to work out pixel overflow and collision. Maybe store the 
-		//          results to replace the sprSearch array I have above.
+    for(int scrX = scrXStart; scrX < scrXEnd; scrX++) {
+        //          TODO: Use a similar algorithm in endLine to work out pixel overflow and collision. Maybe store the 
+        //          results to replace the sprSearch array I have above.
 
-		int bgX = (scrX + (255 - bg_x_scroll)) % (32 * 8);
-		int xTile = bgX / 8;
-		int xFine = bgX % 8;
-		uint16_t tile_info_addr = (name_tab_base() + (yTile * 64) + (xTile * 2)) & 0x3fff;
-		tile_info_t tile_info;
-		tile_info.bytes.byte1 = vram.at(tile_info_addr);
-		tile_info.bytes.byte2 = vram.at(tile_info_addr + 1);
-		//          if(tile_info.fields.priority || 
-		//          TODO: Continue using this and lineBuffer to decide how to draw the background
-		uint16_t tile_addr = (bg_tile_base() + 32 * tile_info.fields.tile_num) & 0x3fff;
-		int tileLine = yFine;
-		if(tile_info.fields.vflip) {
-			tileLine = 7 - yFine;
-		}
-		uint8_t byte0 = vram.at(tile_addr + tileLine * 4);
-		uint8_t byte1 = vram.at(tile_addr + tileLine * 4 + 1);
-		uint8_t byte2 = vram.at(tile_addr + tileLine * 4 + 2);
-		uint8_t byte3 = vram.at(tile_addr + tileLine * 4 + 3);
+        int bgX = (scrX + (255 - bg_x_scroll)) % (32 * 8);
+        int xTile = bgX / 8;
+        int xFine = bgX % 8;
+        uint16_t tile_info_addr = (name_tab_base() + (yTile * 64) + (xTile * 2)) & 0x3fff;
+        tile_info_t tile_info;
+        tile_info.bytes.byte1 = vram.at(tile_info_addr);
+        tile_info.bytes.byte2 = vram.at(tile_info_addr + 1);
+        //          if(tile_info.fields.priority || 
+        //          TODO: Continue using this and lineBuffer to decide how to draw the background
+        uint16_t tile_addr = (bg_tile_base() + 32 * tile_info.fields.tile_num) & 0x3fff;
+        int tileLine = yFine;
+        if(tile_info.fields.vflip) {
+            tileLine = 7 - yFine;
+        }
+        uint8_t byte0 = vram.at(tile_addr + tileLine * 4);
+        uint8_t byte1 = vram.at(tile_addr + tileLine * 4 + 1);
+        uint8_t byte2 = vram.at(tile_addr + tileLine * 4 + 2);
+        uint8_t byte3 = vram.at(tile_addr + tileLine * 4 + 3);
 
-		uint32_t mask = 0x80>>xFine;
-		uint32_t indexShift = 7-xFine;
-		if(tile_info.fields.hflip) {
-			mask = 0x01<<xFine;
-			indexShift = xFine;
-		}
+        uint32_t mask = 0x80>>xFine;
+        uint32_t indexShift = 7-xFine;
+        if(tile_info.fields.hflip) {
+            mask = 0x01<<xFine;
+            indexShift = xFine;
+        }
 
-		// Actual background pixel-drawing
-		// Find color of correct background tile
-		int color_index = (((mask & byte0) + 2*(mask & byte1) + 4*(mask & byte2) + 8*(mask&byte3)) >> indexShift) + 16 * tile_info.fields.palnum;
+        // Actual background pixel-drawing
+        // Find color of correct background tile
+        int color_index = (((mask & byte0) + 2*(mask & byte1) + 4*(mask & byte2) + 8*(mask&byte3)) >> indexShift) + 16 * tile_info.fields.palnum;
 
-		//
-		if(lineBuffer[scrX] && !tile_info.fields.priority) color_index = lineBuffer[scrX];
-		if(!color_index) color_index = 16 + bg_fg_col.fields.background;
+        //
+        if(lineBuffer[scrX] && !tile_info.fields.priority) color_index = lineBuffer[scrX];
+        if(!color_index) color_index = 16 + bg_fg_col.fields.background;
 
-		if(vdpMode == systemType::masterSystem) {
-			setPixelSMS(buffer, scrX, scrY, color_index);
-		}
-		else if(vdpMode == systemType::gameGear) {
-			setPixelGG(buffer, scrX - scrXStart, scrY - scrYStart, color_index);
-		}
-	}
+        if(vdpMode == systemType::masterSystem) {
+            setPixelSMS(buffer, scrX, scrY, color_index);
+        }
+        else if(vdpMode == systemType::gameGear) {
+            setPixelGG(buffer, scrX - scrXStart, scrY - scrYStart, color_index);
+        }
+    }
 }
 
 std::vector<uint8_t> vdpMS::getDebugRender() {
@@ -380,7 +390,7 @@ std::vector<uint8_t> vdpMS::getDBG2Render() {
     }
     return buffer;
 }
- 
+
 std::vector<uint8_t> vdpMS::getDBM4Render() {
     std::vector<uint8_t> buffer(512 * 512 * 3, 0);
     for(int tileRow = 0; tileRow < 32; tileRow++) {
@@ -416,7 +426,7 @@ std::array<uint8_t, 8> vdpMS::getM4TileLine(uint16_t tileAddr, uint8_t row) {
     uint8_t data1 = vram.at(tileAddr + row * 4 + 1);
     uint8_t data2 = vram.at(tileAddr + row * 4 + 2);
     uint8_t data3 = vram.at(tileAddr + row * 4 + 3);
-   
+
     uint8_t mask = 128;
     for(int x = 0; x < 8; x++) {
         retval[x] = ((data0 & mask) >> (7 - x)) + 2 * ((data1 & mask) >> (7 - x)) + 4 * ((data2 & mask) >> (7 - x)) + 8 * ((data3 & mask) >> (7 - x));
@@ -427,7 +437,7 @@ std::array<uint8_t, 8> vdpMS::getM4TileLine(uint16_t tileAddr, uint8_t row) {
 
 void vdpMS::setPixelSG(std::vector<uint8_t>& buffer, int x, int y, int index) {
     if(x < 0 || x >= curXRes || y < 0 || y >= curYRes) return;
-	//std::cout<<"y: "<<y<<" x: "<<x<<"\n";
+    //std::cout<<"y: "<<y<<" x: "<<x<<"\n";
     buffer[y * 256 * 3 + 3 * x + 0] = tms_palette[index * 3 + 2];
     buffer[y * 256 * 3 + 3 * x + 1] = tms_palette[index * 3 + 1];
     buffer[y * 256 * 3 + 3 * x + 2] = tms_palette[index * 3 + 0];
@@ -529,9 +539,9 @@ void vdpMS::endLine(uint64_t lineNum) {
     uint64_t line = lineNum % getFrameLines();
     curLine = line; //VCounter
     if(line == 191) {
-		scr_int_active = true;
-		status.fields.vblank_flag = 1;
-	}
+        scr_int_active = true;
+        status.fields.vblank_flag = 1;
+    }
     if(line < 192 && line_int_cur) {
         line_int_cur--;
     }
@@ -542,7 +552,7 @@ void vdpMS::endLine(uint64_t lineNum) {
     else if(line >= 192) {
         line_int_cur = line_interrupt;
     }
-	renderLine(line, buffer);
+    renderLine(line, buffer);
 }
 
 void vdpMS::writeByte(uint8_t port, uint8_t val, uint64_t cycle) {
@@ -673,11 +683,11 @@ uint8_t vdpMS::readStatus(uint64_t cycle) {
     //line_int_active = false;
     //return temp.val;
     //return 0x80;
-	uint8_t retval = status.val;
-	status.fields.collision_flag = 0;
-	status.fields.overflow_flag = 0;
-	status.fields.vblank_flag = 0;
-	return retval;
+    uint8_t retval = status.val;
+    status.fields.collision_flag = 0;
+    status.fields.overflow_flag = 0;
+    status.fields.vblank_flag = 0;
+    return retval;
 }
 
 uint8_t vdpMS::readVCounter(uint64_t cycle) {
