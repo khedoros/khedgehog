3830ebe707c0a5742f0aed4c1b9f73d6d73719cf
khedoros <khedoros@gmail.com>
2021-04-09 12:25:33 -0600

    YM2413 improvements
    - Finished applyRegister code
    - Fixed phase counter to match 19 bits instead of 18
    - Reworked the instrument+op+channel distinction
    - Skip channels dedicated to rhythm when it's active
    - Output is much cleaner now!

--- apu/yamahaYm2413.cpp
+++ apu/yamahaYm2413.cpp
@@ -6,10 +6,10 @@ YamahaYm2413::YamahaYm2413(std::shared_ptr<config>& conf) : apu(conf), curReg(0)
     buffer.fill(0);
 
     if(cfg->getSystemRegion() == systemRegion::pal) {
-		sampleCnt = 882;
-		ticksPerSample = 5.03;
-	}
-	else {
+        sampleCnt = 882;
+        ticksPerSample = 5.03;
+    }
+    else {
         sampleCnt = 735;
         ticksPerSample = 5.07;
     }
@@ -17,6 +17,49 @@ YamahaYm2413::YamahaYm2413(std::shared_ptr<config>& conf) : apu(conf), curReg(0)
     for(double i=0.0;i<512.0;i+=1.0) {
         //std::cout<<"i: "<<i<<" sine: "<<sin((i+.5)*M_PI/512)<<"\n";
         sine[int(i)] = sin((i+.5)*M_PI/512);
+    }
+
+    for(int instrument=0;instrument<16;instrument++) {
+        int tableBase = instrument * 8;
+        int reg0 = romInst[tableBase + 0];
+        int reg1 = romInst[tableBase + 1];
+        int reg2 = romInst[tableBase + 2];
+        int reg3 = romInst[tableBase + 3];
+        int reg4 = romInst[tableBase + 4];
+        int reg5 = romInst[tableBase + 5];
+        int reg6 = romInst[tableBase + 6];
+        int reg7 = romInst[tableBase + 7];
+        inst[instrument].amMod = (reg0 & 0x80);
+        inst[instrument].vibMod = (reg0 & 0x40);
+        inst[instrument].sustMod = (reg0 & 0x20);
+        inst[instrument].keyScaleRateMod = (reg0 & 0x10);
+        inst[instrument].multMod = (reg0 & 0x0f);
+
+        inst[instrument].amCar = (reg1 & 0x80);
+        inst[instrument].vibCar = (reg1 & 0x40);
+        inst[instrument].sustCar = (reg1 & 0x20);
+        inst[instrument].keyScaleRateCar = (reg1 & 0x10);
+        inst[instrument].multCar = (reg1 & 0x0f);
+
+        inst[instrument].keyScaleLevelMod = (reg2>>6);
+        inst[instrument].totalLevelMod = (reg2 & 0x3f);
+
+        inst[instrument].keyScaleLevelCar = (reg3>>6);
+        inst[instrument].waveformCar = (reg3 & 0x10);
+        inst[instrument].waveformMod = (reg3 & 0x08);
+        inst[instrument].feedbackMod = (reg3 & 0x07);
+
+        inst[instrument].attackMod = (reg4 & 0xf0)>>4;
+        inst[instrument].decayMod = (reg4 & 0x0f);
+
+        inst[instrument].attackCar = (reg5 & 0xf0)>>4;
+        inst[instrument].decayCar = (reg5 & 0x0f);
+
+        inst[instrument].sustainLevelMod = (reg6 & 0xf0)>>4;
+        inst[instrument].releaseMod = (reg6 & 0x0f);
+
+        inst[instrument].sustainLevelCar = (reg7 & 0xf0)>>4;
+        inst[instrument].releaseCar = (reg7 & 0x0f);
     }
 }
 void YamahaYm2413::mute(bool) {}
@@ -43,69 +86,111 @@ uint8_t YamahaYm2413::readRegister(uint8_t port) {
 }
 
 void YamahaYm2413::applyRegister(std::pair<uint8_t, uint8_t>& write) {
-	uint8_t reg = write.first;
-	uint8_t val = write.second;
-    if(reg >= 0x10 && reg <= 0x18) {
+    uint8_t reg = write.first;
+    uint8_t val = write.second;
+    if(reg < 8) {
+        switch(reg) {
+            case 0:
+                inst[0].amMod = (val & 0x80);
+                inst[0].vibMod = (val & 0x40);
+                inst[0].sustMod = (val & 0x20);
+                inst[0].keyScaleRateMod = (val & 0x10);
+                inst[0].multMod = (val & 0x0f);
+                break;
+            case 1:
+                inst[0].amCar = (val & 0x80);
+                inst[0].vibCar = (val & 0x40);
+                inst[0].sustCar = (val & 0x20);
+                inst[0].keyScaleRateCar = (val & 0x10);
+                inst[0].multCar = (val & 0x0f);
+                break;
+            case 2:
+                inst[0].keyScaleLevelMod = (val>>6);
+                inst[0].totalLevelMod = (val & 0x3f);
+                break;
+            case 3:
+                inst[0].keyScaleLevelCar = (val>>6);
+                inst[0].waveformCar = (val & 0x10);
+                inst[0].waveformMod = (val & 0x08);
+                inst[0].feedbackMod = (val & 0x07);
+                break;
+            case 4:
+                inst[0].attackMod = (val & 0xf0)>>4;
+                inst[0].decayMod = (val & 0x0f);
+                break;
+            case 5:
+                inst[0].attackCar = (val & 0xf0)>>4;
+                inst[0].decayCar = (val & 0x0f);
+                break;
+            case 6:
+                inst[0].sustainLevelMod = (val & 0xf0)>>4;
+                inst[0].releaseMod = (val & 0x0f);
+                break;
+            case 7:
+                inst[0].sustainLevelCar = (val & 0xf0)>>4;
+                inst[0].releaseCar = (val & 0x0f);
+                break;
+        }
+    }
+    else if(reg == 0x0e) {
+        rhythm = val & 0x20;
+        bass_trigger = val & 0x10;
+        snare_trigger = val & 0x8;
+        tom_trigger = val & 0x4;
+        top_cymbal_trigger = val & 0x2;
+        high_hat_trigger = val & 0x1;
+    }
+    else if(reg >= 0x10 && reg <= 0x18) {
         int num = (reg & 0x0f);
-        int modOp = (num / 3) * 6;
-        int carOp = (num / 3) * 6 + 3;
         chan[num].fNum &= 0x100;
         chan[num].fNum |= val;
-        op[modOp].phaseInc = 256 * chan[num].octave + chan[num].fNum;
-        //if(!op[modOp].mult) op[modOp].phaseInc /= 2;
-        //else op[modOp.phaseInc] *= multVal[op[modOp].mult];
-        op[carOp].phaseInc = 256 * chan[num].octave + chan[num].fNum;
-        //if(!op[carOp].mult) op[carOp].phaseInc /= 2;
-        //else op[carOp.phaseInc] *= multVal[op[carOp].mult];
+        chan[num].modOp.phaseInc = ((chan[num].fNum * multVal[inst[chan[num].modOp.instNum].multMod]) << chan[num].octave) >> 1;
+        chan[num].carOp.phaseInc = ((chan[num].fNum * multVal[inst[chan[num].carOp.instNum].multCar]) << chan[num].octave) >> 1;
     }
     else if(reg >= 0x20 && reg <= 0x28) {
         int num = (reg & 0x0f);
-        int modOp = (num / 3) * 6;
-        int carOp = (num / 3) * 6 + 3;
         chan[num].fNum &= 0xff;
         chan[num].fNum |= ((val&0x01)<<8);
         chan[num].octave = ((val>>1) & 0x07);
         chan[num].sustain = ((val>>5) & 0x01);
         chan[num].trigger = ((val>>4) & 0x01);
-        op[modOp].phaseInc = 256 * chan[num].octave + chan[num].fNum;
-        //if(!op[modOp].mult) op[modOp].phaseInc /= 2;
-        //else op[modOp.phaseInc] *= multVal[op[modOp].mult];
-        op[carOp].phaseInc = 256 * chan[num].octave + chan[num].fNum;
-        //if(!op[carOp].mult) op[carOp].phaseInc /= 2;
-        //else op[carOp.phaseInc] *= multVal[op[carOp].mult];
+        chan[num].modOp.phaseInc = ((chan[num].fNum * multVal[inst[chan[num].modOp.instNum].multMod]) << chan[num].octave) >> 1;
+        chan[num].carOp.phaseInc = ((chan[num].fNum * multVal[inst[chan[num].carOp.instNum].multCar]) << chan[num].octave) >> 1;
     }
 }
 
 void YamahaYm2413::setStereo(uint8_t) {}
 
 std::array<int16_t, 882 * 2>& YamahaYm2413::getSamples() {
-	if(cfg->getSystemType() != systemType::masterSystem) return buffer;
+    if(cfg->getSystemType() != systemType::masterSystem) return buffer;
 
     buffer.fill(0);
 
-	int curIndex = 0;
+    int curIndex = 0;
     for(int i=0;i<sampleCnt;i++) {
-	// TODO: Super-hacky. Spread register writes evenly through the frame.
-	// Fixing it properly would require the CPU to send the write cycle to the memmap, so that writes could be
-	// timed properly in the frame. But...this works, so far. Easy way out.
-	/**/ float percent = float(i) / float(sampleCnt);
-	/**/ if(int(percent * writeIndex) >= curIndex) {
-	/**/    applyRegister(regWrites[curIndex++]);
-	/**/ }
+    // TODO: Super-hacky. Spread register writes evenly through the frame.
+    // Fixing it properly would require the CPU to send the write cycle to the memmap, so that writes could be
+    // timed properly in the frame. But...this works, so far. Easy way out.
+    /**/ float percent = float(i) / float(sampleCnt);
+    /**/ if(int(percent * writeIndex) >= curIndex) {
+    /**/    applyRegister(regWrites[curIndex++]);
+    /**/ }
+        int chanMax = (rhythm)?6:9;
         for(int ch=0;ch<9;ch++) {
             if(chan[ch].trigger) {
                 int modOp = (ch / 3) * 6;
                 int carOp = (ch / 3) * 6 + 3;
-                op[modOp].phaseCnt += op[modOp].phaseInc;
-                op[carOp].phaseCnt += op[carOp].phaseInc;
-                int phase = op[carOp].phaseCnt / 256;
+                chan[ch].modOp.phaseCnt += chan[ch].modOp.phaseInc;
+                chan[ch].carOp.phaseCnt += chan[ch].carOp.phaseInc;
+                int phase = chan[ch].carOp.phaseCnt / 512;
                 if(phase >= 512) buffer[i] -= (sine[phase - 512] * 512);
                 else buffer[i] += (sine[phase] * 512);
             }
         }
-	}
+        if(rhythm) { // TODO: handle the 5 rhythm instruments
+        }
+    }
 
-    // TODO: generate a frame of audio (draw the rest of the bleeping owl)
     writeIndex = 0;
     return buffer;
 }
--- apu/yamahaYm2413.h
+++ apu/yamahaYm2413.h
@@ -12,14 +12,14 @@ public:
     void setStereo(uint8_t) override;
     std::array<int16_t, 882 * 2>& getSamples() override;
 private:
-	void applyRegister(std::pair<uint8_t, uint8_t>& write);
+    void applyRegister(std::pair<uint8_t, uint8_t>& write);
     std::array<int16_t, 882 * 2> buffer;
     uint8_t curReg;
     uint8_t statusVal;
 
-	std::shared_ptr<config> cfg;
-	int sampleCnt;
-	double ticksPerSample;
+    std::shared_ptr<config> cfg;
+    int sampleCnt;
+    double ticksPerSample;
 
     unsigned int writeIndex;
     std::array<std::pair<uint8_t, uint8_t>, 100> regWrites;
@@ -38,41 +38,63 @@ private:
     unsigned high_hat_vol:4; //0x37-high
     unsigned top_cym_vol:4; //0x38-low
 
-	enum adsrPhase {
-		dampen,  //End of previous note, behaves like a base decay rate of 12, adjusted by rate_key_scale
-		attack,  //New note rising after key-on
-		decay,   //Initial fade to sustain level after reaching max volume
-		sustain, //Level to hold at until key-off, or level at which to transition from decay to release phase
-		release, //key-off for non-percussive notes, sustain for percussive
+    enum adsrPhase {
+        dampen,  //End of previous note, behaves like a base decay rate of 12, adjusted by rate_key_scale
+        attack,  //New note rising after key-on
+        decay,   //Initial fade to sustain level after reaching max volume
+        sustain, //Level to hold at until key-off, or level at which to transition from decay to release phase
+        release, //key-off for non-percussive notes, sustain for percussive
         release2 //key-off for percussive notes
-	};
+    };
 
     struct op_t {
-        //reg 0+1
-        bool am;      //tremolo (amplitude variance) @ 3.7Hz
-        bool vibrato; //frequency variance @ 6.4Hz
-        bool sustain;        //1=sustained tone, 0=no sustain period
-        bool rate_key_scale; //KSR: modify ADSR rate based on frequency
-        unsigned mult:4;     //frequency multiplier, 1 of 3 elements that define the frequency
+        unsigned instNum:4;
+        unsigned instVol:4;
+        unsigned phaseInc:19;
+        unsigned phaseCnt:19;
+        adsrPhase envPhase;
+        //TODO: Add something about an envelope counter
 
-        //reg 2+3
-        unsigned level_key_scale:2; //KSL: modify volume based on frequency
-        unsigned mod_total_level:6; //modulation attenuation
-        bool waveform;              //0=sine, 1=only positive
-        unsigned feedback:3;        //feedback level into modulator operator (I think)
+    };
 
-        //ADSR reg 4-7
-        unsigned attack:4;
-        unsigned decay:4;
-        unsigned sustain_level:4;
-        unsigned release:4;
+    struct inst_t {
+        //reg 0
+        bool amMod;           //tremolo (amplitude variance) @ 3.7Hz
+        bool vibMod;          //frequency variance @ 6.4Hz
+        bool sustMod;         //1=sustained tone, 0=no sustain period
+        bool keyScaleRateMod; //KSR: modify ADSR rate based on frequency
+        unsigned multMod:4;   //frequency multiplier, 1 of 3 elements that define the frequency
 
-        unsigned phaseInc:18;
-		unsigned phaseCnt:18;
-		adsrPhase envPhase;
-		//TODO: Add something about an envelope counter
+        //reg 1
+        bool amCar;           //tremolo (amplitude variance) @ 3.7Hz
+        bool vibCar;          //frequency variance @ 6.4Hz
+        bool sustCar;         //1=sustained tone, 0=no sustain period
+        bool keyScaleRateCar; //KSR: modify ADSR rate based on frequency
+        unsigned multCar:4;   //frequency multiplier, 1 of 3 elements that define the frequency
 
-    } op[18];
+        //reg 2
+        unsigned keyScaleLevelMod:2; //KSL: modify volume based on frequency
+        unsigned totalLevelMod:6;    //modulation attenuation
+
+        //reg 3
+        unsigned keyScaleLevelCar:2; //KSL: modify volume based on frequency
+        bool waveformMod;            //0=sine, 1=only positive
+        bool waveformCar;            //0=sine, 1=only positive
+        unsigned feedbackMod:3;      //feedback level into modulator operator (I think)
+
+        //reg 4
+        unsigned attackMod:4;
+        unsigned decayMod:4;
+        //reg 5
+        unsigned attackCar:4;
+        unsigned decayCar:4;
+        //reg 6
+        unsigned sustainLevelMod:4;
+        unsigned releaseMod:4;
+        //reg 7
+        unsigned sustainLevelCar:4;
+        unsigned releaseCar:4;
+    } inst[16];
 
     struct chan_t {
         unsigned fNum:9; // 2nd of 3 elements that define the frequency
@@ -81,31 +103,31 @@ private:
         unsigned octave:3; //3rd element that defines the frequency
         unsigned instrument:4; //instrument choice 0=custom, 1-f=built-in instruments
         unsigned volume:4; // volume for the channel
-        op_t mod_op;
-		op_t car_op;
+        op_t modOp;
+        op_t carOp;
     } chan[9];
 
-    static constexpr uint8_t multVal[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 12, 12, 15, 15};
+    static constexpr uint8_t multVal[16] = {1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 20, 24, 24, 30, 30};
 
-	static constexpr uint8_t instruments[8*(1+15+3)] = {
-		0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00, // 0: User
-		0x71,0x61,0x1e,0x17,0xd0,0x78,0x00,0x17, // 1: Violin
-		0x13,0x41,0x1a,0x0d,0xd8,0xf7,0x23,0x13, // 2: Guitar
-		0x13,0x01,0x99,0x00,0xf2,0xc4,0x21,0x23, // 3: Piano
-		0x11,0x61,0x0e,0x07,0x8d,0x64,0x70,0x27, // 4: Flute
-		0x32,0x21,0x1e,0x06,0xe1,0x76,0x01,0x28, // 5: Clarinet
-		0x31,0x22,0x16,0x05,0xe0,0x71,0x00,0x18, // 6: Oboe
-		0x21,0x61,0x1d,0x07,0x82,0x81,0x11,0x07, // 7: Trumpet
-		0x33,0x21,0x2d,0x13,0xb0,0x70,0x00,0x07, // 8: Organ
-		0x61,0x61,0x1b,0x06,0x64,0x65,0x10,0x17, // 9: Horn
-		0x41,0x61,0x0b,0x18,0x85,0xf0,0x81,0x07, // A: Synthesizer
-		0x33,0x01,0x83,0x11,0xea,0xef,0x10,0x04, // B: Harpsichord
-		0x17,0xc1,0x24,0x07,0xf8,0xf8,0x22,0x12, // C: Vibraphone
-		0x61,0x50,0x0c,0x05,0xd2,0xf5,0x40,0x42, // D: Synthsizer Bass
-		0x01,0x01,0x55,0x03,0xe9,0x90,0x03,0x02, // E: Acoustic Bass
-		0x41,0x41,0x89,0x03,0xf1,0xe4,0xc0,0x13, // F: Electric Guitar
-		0x01,0x01,0x18,0x0f,0xdf,0xf8,0x6a,0x6d, // R: Bass Drum (from VRC7)
-		0x01,0x01,0x00,0x00,0xc8,0xd8,0xa7,0x68, // R: High-Hat(M) / Snare Drum(C) (from VRC7)
-		0x05,0x01,0x00,0x00,0xf8,0xaa,0x59,0x55, // R: Tom-tom(M) / Top Cymbal(C) (from VRC7)
-	};
+    static constexpr uint8_t romInst[8*(1+15+3)] = {
+        0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00, // 0: User
+        0x71,0x61,0x1e,0x17,0xd0,0x78,0x00,0x17, // 1: Violin
+        0x13,0x41,0x1a,0x0d,0xd8,0xf7,0x23,0x13, // 2: Guitar
+        0x13,0x01,0x99,0x00,0xf2,0xc4,0x21,0x23, // 3: Piano
+        0x11,0x61,0x0e,0x07,0x8d,0x64,0x70,0x27, // 4: Flute
+        0x32,0x21,0x1e,0x06,0xe1,0x76,0x01,0x28, // 5: Clarinet
+        0x31,0x22,0x16,0x05,0xe0,0x71,0x00,0x18, // 6: Oboe
+        0x21,0x61,0x1d,0x07,0x82,0x81,0x11,0x07, // 7: Trumpet
+        0x33,0x21,0x2d,0x13,0xb0,0x70,0x00,0x07, // 8: Organ
+        0x61,0x61,0x1b,0x06,0x64,0x65,0x10,0x17, // 9: Horn
+        0x41,0x61,0x0b,0x18,0x85,0xf0,0x81,0x07, // A: Synthesizer
+        0x33,0x01,0x83,0x11,0xea,0xef,0x10,0x04, // B: Harpsichord
+        0x17,0xc1,0x24,0x07,0xf8,0xf8,0x22,0x12, // C: Vibraphone
+        0x61,0x50,0x0c,0x05,0xd2,0xf5,0x40,0x42, // D: Synthsizer Bass
+        0x01,0x01,0x55,0x03,0xe9,0x90,0x03,0x02, // E: Acoustic Bass
+        0x41,0x41,0x89,0x03,0xf1,0xe4,0xc0,0x13, // F: Electric Guitar
+        0x01,0x01,0x18,0x0f,0xdf,0xf8,0x6a,0x6d, // R: Bass Drum (from VRC7)
+        0x01,0x01,0x00,0x00,0xc8,0xd8,0xa7,0x68, // R: High-Hat(M) / Snare Drum(C) (from VRC7)
+        0x05,0x01,0x00,0x00,0xf8,0xaa,0x59,0x55, // R: Tom-tom(M) / Top Cymbal(C) (from VRC7)
+    };
 };
